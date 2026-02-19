96c7f87f755ec938902bb388250febf479c91ada
darren yim <darrenyxj@gmail.com>
2026-02-19 21:51:16 +0100

    fix: add lock as rng is not thread safe

--- internal/room/code.go
+++ internal/room/code.go
@@ -3,16 +3,29 @@ package room
 import (
 	"math/rand"
 	"strings"
+	"sync"
+	"time"
 )
 
 const validChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
+var (
+	// deep dive required here, added lock for now
+	rng   = rand.New(rand.NewSource(time.Now().UnixNano()))
+	rngMu sync.Mutex
+)
+
 func generateCode() string {
+	rngMu.Lock()
+	defer rngMu.Unlock()
+
 	sb := strings.Builder{}
+
 	for i := 0; i < 6; i++ {
 		// validChars contains only ASCII characters(single byte chars)
 		// so byte indexing is safe
-		sb.WriteByte(validChars[rand.Intn(len(validChars))])
+		sb.WriteByte(validChars[rng.Intn(len(validChars))])
 	}
+
 	return sb.String()
 }
